d95a6612755fd4620aa581cc9d6e2d7a2502a20a
Tilo Sloboda <tilo.sloboda@gmail.com>
2002-11-06 03:49:52 -0800

    ready for 0.2 release

--- lib/id3.rb
+++ lib/id3.rb
@@ -2,7 +2,9 @@
 # id3v2.rb     Ruby Module for handling the following ID3-tag versions:
 #                  ID3v1.0 , ID3v1.1,  ID3v2.2.0, ID3v2.3.0, ID3v2.4.0
 # 
-# Copyright:   Tilo Sloboda <tilo@unixgods.org> , created 12 October 2002
+# Copyright (C) 2002 by Tilo Sloboda <tilo@unixgods.org> 
+#
+# created:      12 October 2002
 #
 # Docs:   http://www.id3.org/id3v2-00.txt
 #         http://www.id3.org/id3v2.3.0.txt
@@ -84,10 +86,12 @@
 #
 ################################################################################
 
-
+# ==============================================================================
+# EXTENDING CLASS STRING
+# ==============================================================================
 
 class String
-  # ----------------------------------------------------------------------
+  #
   # prints out a good'ol hexdump of the data contained in the string
 
   def hexdump(verbose = 0)
@@ -136,22 +140,28 @@ class String
 
 end
 
+# ==============================================================================
+# MODULE ID3
+# ==============================================================================
 
 module ID3
-
-  # check if file has a ID3-tag and which version it is
-  # 
-  # NOTE: file might be tagged twice! :(
-  
   # ----------------------------------------------------------------------------
   #    CONSTANTS
   # ----------------------------------------------------------------------------
+  @@RCSid = '$Id: id3.rb,v 1.1 2002/10/28 03:02:10 tilo Exp tilo $'
+
   ID3v1tagSize = 128;    # ID3v1 and ID3v1.1 have fixed size tags at end of file
   ID3v2headerSize = 10;
 
+  # Struct for storing internal information about ID3v2 frames, and how to handle them
+  #
+  ID3frame = Struct.new("ID3frame", :name, :headerStartX,  :dataStartX, :dataEndX, :flags)
+  ID3frameHandler = Struct.new("ID3frameHandler", :name, :unpack, :pack)
+  
   # different versions of ID3 tags, support different fields.
   # See: http://www.unixgods.org/~tilo/ID3v2_frames_comparison.txt
 
+  
   SUPPORTED_SYMBOLS = {
     "1.0"   => {"ARTIST"=>33..62 , "ALBUM"=>63..92 ,"TITLE"=>3..32,
                 "YEAR"=>93..96 , "COMMENT"=>97..126,"GENREID"=>127
@@ -161,7 +171,12 @@ module ID3
                 "GENREID"=>127
                }  ,
 
-
+    #
+    # NOTE: values for hash need to be different for ID3v2.x!!
+    #       What i really want to do is to create the following entries dynamically 
+    #       at startup - so we can have Structs for each entry, which contain the name
+    #       of the field-TAG as well as pointers to the pack() and unpack() functions
+    
     "2.2.0" => {"CONTENTGROUP"=>"TT1", "TITLE"=>"TT2", "SUBTITLE"=>"TT3",
                 "ARTIST"=>"TP1", "BAND"=>"TP2", "CONDUCTOR"=>"TP3", "MIXARTIST"=>"TP4",
                 "COMPOSER"=>"TCM", "LYRICIST"=>"TXT", "LANGUAGE"=>"TLA", "CONTENTTYPE"=>"TCO",
@@ -173,34 +188,98 @@ module ID3
                 "PLAYLISTDELAY"=>"TDY", "INITIALKEY"=>"TKE", "ORIGALBUM"=>"TOT",
                 "ORIGFILENAME"=>"TOF", "ORIGARTIST"=>"TOA", "ORIGLYRICIST"=>"TOL",
                 "USERTEXT"=>"TXX", 
-                "WWWAUDIOFILE"=>"WAF"
-                #
-                ### to be continued.. 
+                "WWWAUDIOFILE"=>"WAF", "WWWARTIST"=>"WAR", "WWWAUDIOSOURCE"=>"WAS",
+                "WWWCOMMERCIALINFO"=>"WCM", "WWWCOPYRIGHT"=>"WCP", "WWWPUBLISHER"=>"WPB",
+                "WWWUSER"=>"WXX", "UNIQUEFILEID"=>"UFI",
+                "INVOLVEDPEOPLE"=>"IPL", "UNSYNCEDLYRICS"=>"ULT", "COMMENT"=>"COM",
+                "CDID"=>"MCI", "EVENTTIMING"=>"ETC", "MPEGLOOKUP"=>"MLL",
+                "SYNCEDTEMPO"=>"STC", "SYNCEDLYRICS"=>"SLT", "VOLUMEADJ"=>"RVA",
+                "EQUALIZATION"=>"EQU", "REVERB"=>"REV", "PICTURE"=>"PIC",
+                "GENERALOBJECT"=>"GEO", "PLAYCOUNTER"=>"CNT", "POPULARIMETER"=>"POP",
+                "BUFFERSIZE"=>"BUF", "CRYPTEDMETA"=>"CRM", "AUDIOCRYPTO"=>"CRA",
+                "LINKED"=>"LNK"
                } ,
-    "2.3.0" => {},
-    "2.4.0" => {}
+               
+    "2.3.0" => {"CONTENTGROUP"=>"TIT1", "TITLE"=>"TIT2", "SUBTITLE"=>"TIT3",
+                "ARTIST"=>"TPE1", "BAND"=>"TPE2", "CONDUCTOR"=>"TPE3", "MIXARTIST"=>"TPE4",
+                "COMPOSER"=>"TCOM", "LYRICIST"=>"TEXT", "LANGUAGE"=>"TLAN", "CONTENTTYPE"=>"TCON",
+                "ALBUM"=>"TALB", "TRACKNUM"=>"TRCK", "PARTINSET"=>"TPOS", "ISRC"=>"TSRC",
+                "DATE"=>"TDAT", "YEAR"=>"TYER", "TIME"=>"TIME", "RECORDINGDATES"=>"TRDA",
+                "ORIGYEAR"=>"TORY", "SIZE"=>"TSIZ", 
+                "BPM"=>"TBPM", "MEDIATYPE"=>"TMED", "FILETYPE"=>"TFLT", "COPYRIGHT"=>"TCOP",
+                "PUBLISHER"=>"TPUB", "ENCODEDBY"=>"TENC", "ENCODERSETTINGS"=>"TSSE",
+                "SONGLEN"=>"TLEN", "PLAYLISTDELAY"=>"TDLY", "INITIALKEY"=>"TKEY",
+                "ORIGALBUM"=>"TOAL", "ORIGFILENAME"=>"TOFN", "ORIGARTIST"=>"TOPE",
+                "ORIGLYRICIST"=>"TOLY", "FILEOWNER"=>"TOWN", "NETRADIOSTATION"=>"TRSN",
+                "NETRADIOOWNER"=>"TRSO", "USERTEXT"=>"TXXX",
+                "WWWAUDIOFILE"=>"WOAF", "WWWARTIST"=>"WOAR", "WWWAUDIOSOURCE"=>"WOAS",
+                "WWWCOMMERCIALINFO"=>"WCOM", "WWWCOPYRIGHT"=>"WCOP", "WWWPUBLISHER"=>"WPUB",
+                "WWWRADIOPAGE"=>"WORS", "WWWPAYMENT"=>"WPAY", "WWWUSER"=>"WXXX", "UNIQUEFILEID"=>"UFID",
+                "INVOLVEDPEOPLE"=>"IPLS", 
+                "UNSYNCEDLYRICS"=>"USLT", "COMMENT"=>"COMM", "TERMSOFUSE"=>"USER",
+                "CDID"=>"MCDI", "EVENTTIMING"=>"ETCO", "MPEGLOOKUP"=>"MLLT",
+                "SYNCEDTEMPO"=>"SYTC", "SYNCEDLYRICS"=>"SYLT", 
+                "VOLUMEADJ"=>"RVAD", "EQUALIZATION"=>"EQUA", 
+                "REVERB"=>"RVRB", "PICTURE"=>"APIC", "GENERALOBJECT"=>"GEOB",
+                "PLAYCOUNTER"=>"PCNT", "POPULARIMETER"=>"POPM", "BUFFERSIZE"=>"RBUF",
+                "AUDIOCRYPTO"=>"AENC", "LINKEDINFO"=>"LINK", "POSITIONSYNC"=>"POSS",
+                "COMMERCIAL"=>"COMR", "CRYPTOREG"=>"ENCR", "GROUPINGREG"=>"GRID", 
+                "PRIVATE"=>"PRIV"
+               } ,
+               
+    "2.4.0" => {"CONTENTGROUP"=>"TIT1", "TITLE"=>"TIT2", "SUBTITLE"=>"TIT3",
+                "ARTIST"=>"TPE1", "BAND"=>"TPE2", "CONDUCTOR"=>"TPE3", "MIXARTIST"=>"TPE4",
+                "COMPOSER"=>"TCOM", "LYRICIST"=>"TEXT", "LANGUAGE"=>"TLAN", "CONTENTTYPE"=>"TCON",
+                "ALBUM"=>"TALB", "TRACKNUM"=>"TRCK", "PARTINSET"=>"TPOS", "ISRC"=>"TSRC",
+                "RECORDINGTIME"=>"TDRC", "ORIGRELEASETIME"=>"TDOR",
+                "BPM"=>"TBPM", "MEDIATYPE"=>"TMED", "FILETYPE"=>"TFLT", "COPYRIGHT"=>"TCOP",
+                "PUBLISHER"=>"TPUB", "ENCODEDBY"=>"TENC", "ENCODERSETTINGS"=>"TSSE",
+                "SONGLEN"=>"TLEN", "PLAYLISTDELAY"=>"TDLY", "INITIALKEY"=>"TKEY",
+                "ORIGALBUM"=>"TOAL", "ORIGFILENAME"=>"TOFN", "ORIGARTIST"=>"TOPE",
+                "ORIGLYRICIST"=>"TOLY", "FILEOWNER"=>"TOWN", "NETRADIOSTATION"=>"TRSN",
+                "NETRADIOOWNER"=>"TRSO", "USERTEXT"=>"TXXX",
+                "SETSUBTITLE"=>"TSST", "MOOD"=>"TMOO", "PRODUCEDNOTICE"=>"TPRO",
+                "ENCODINGTIME"=>"TDEN", "RELEASETIME"=>"TDRL", "TAGGINGTIME"=>"TDTG",
+                "ALBUMSORTORDER"=>"TSOA", "PERFORMERSORTORDER"=>"TSOP", "TITLESORTORDER"=>"TSOT",
+                "WWWAUDIOFILE"=>"WOAF", "WWWARTIST"=>"WOAR", "WWWAUDIOSOURCE"=>"WOAS",
+                "WWWCOMMERCIALINFO"=>"WCOM", "WWWCOPYRIGHT"=>"WCOP", "WWWPUBLISHER"=>"WPUB",
+                "WWWRADIOPAGE"=>"WORS", "WWWPAYMENT"=>"WPAY", "WWWUSER"=>"WXXX", "UNIQUEFILEID"=>"UFID",
+                "MUSICIANCREDITLIST"=>"TMCL", "INVOLVEDPEOPLE2"=>"TIPL",
+                "UNSYNCEDLYRICS"=>"USLT", "COMMENT"=>"COMM", "TERMSOFUSE"=>"USER",
+                "CDID"=>"MCDI", "EVENTTIMING"=>"ETCO", "MPEGLOOKUP"=>"MLLT",
+                "SYNCEDTEMPO"=>"SYTC", "SYNCEDLYRICS"=>"SYLT", 
+                "VOLUMEADJ2"=>"RVA2", "EQUALIZATION2"=>"EQU2",
+                "REVERB"=>"RVRB", "PICTURE"=>"APIC", "GENERALOBJECT"=>"GEOB",
+                "PLAYCOUNTER"=>"PCNT", "POPULARIMETER"=>"POPM", "BUFFERSIZE"=>"RBUF",
+                "AUDIOCRYPTO"=>"AENC", "LINKEDINFO"=>"LINK", "POSITIONSYNC"=>"POSS",
+                "COMMERCIAL"=>"COMR", "CRYPTOREG"=>"ENCR", "GROUPINGREG"=>"GRID", 
+                "PRIVATE"=>"PRIV",
+                "OWNERSHIP"=>"OWNE", "SIGNATURE"=>"SIGN", "SEEKFRAME"=>"SEEK",
+                "AUDIOSEEKPOINT"=>"ASPI"
+               }
   }
-  # ----------------------------------------------------------------------------
-  #    VARIABLES
-  # ----------------------------------------------------------------------------
   
   # ----------------------------------------------------------------------------
-  #    METHODS
+  #  MODULE FUNCTIONS
   # ----------------------------------------------------------------------------
+  module_function
 
+  # check if file has a ID3-tag and which version it is
+  # 
+  # NOTE: file might be tagged twice! :(
+  
   # ----------------------------------------------------------------------------
   # has_v1_tag? 
   #              returns true if v1.0 or v1.1 tag was found 
 
-  def ID3::has_v1_tag?(filename)
+  def has_v1_tag?(filename)
     hastag     = 0
     
     f = File.open(filename, 'r')
     f.seek(-ID3v1tagSize, IO::SEEK_END)
     if (f.read(3) == "TAG")
-      f.seek(-ID3v1tagSize + 124, IO::SEEK_END)
+      f.seek(-ID3v1tagSize + 125, IO::SEEK_END)
       c = f.getc;                         # this is character 125 of the tag
-  #   print "char = #{c}\n"
       if (c == 0) 
          hastag = "1.1"
       else
@@ -215,7 +294,7 @@ module ID3
   # has_v2_tag? 
   #              returns true if a tag version 2.2.0, 2.3.0 or 2.4.0 was found 
   
-  def ID3::has_v2_tag?(filename)
+  def has_v2_tag?(filename)
     hastag     = 0
     
     f = File.open(filename, 'r')
@@ -237,27 +316,20 @@ class ID3tag < Hash
   # ----------------------------------------------------------------------------
   #    VARIABLES
   # ----------------------------------------------------------------------------
-
-  alias old_set []=
-
-  def []=(key,val)
-     if  SUPPORTED_SYMBOLS[@version].keys.include?(key)
-        old_set(key,val)
-     else 
-        # exception
-        raise ArgumentError, "Incorrect ID3-field \"#{key}\" for ID3 version #{@version}\n" +
-                             "\t\tvalid fields are: " + SUPPORTED_SYMBOLS[@version].keys.join(",") +"\n"
-     end
-  end
-
-  # keys, values, each, etc. come for free with Hash
+  @@framename2symbol= Hash.new
+  @@framename2symbol["1.0"] = ID3::SUPPORTED_SYMBOLS["1.0"].invert
+  @@framename2symbol["1.1"] = ID3::SUPPORTED_SYMBOLS["1.1"].invert
+  @@framename2symbol["2.2.0"] = ID3::SUPPORTED_SYMBOLS["2.2.0"].invert
+  @@framename2symbol["2.3.0"] = ID3::SUPPORTED_SYMBOLS["2.3.0"].invert
+  @@framename2symbol["2.4.0"] = ID3::SUPPORTED_SYMBOLS["2.4.0"].invert
+        
 
   # ----------------------------------------------------------------------------
-  #    PRIVATE METHODS
+  #    PRIVATE METHODS   (need to hide those!)
   # ----------------------------------------------------------------------------
-
+  private
+  
   def initialize
- 
       @version = ""
       @raw     = "";    # the raw ID3 tag
   end
@@ -301,10 +373,55 @@ class ID3tag < Hash
 
     return bytes
   end
+  # ----------------------------------------------------------------------
+  #    UNPACKING and PACKING METHODS
+  # ----------------------------------------------------------------------------
+  def unpack_TXT (frame)
+     i = frame.dataStartX
+     j = frame.dataEndX
+     if @raw[i] == 0
+       return @raw[i+1..j].squeeze(" \000").chomp(" ").chomp("\000")
+     else 
+       return "__UNKNOWN_CODING__"
+     end
+  end
 
   # ----------------------------------------------------------------------------
   #    PUBLIC METHODS
   # ----------------------------------------------------------------------------
+  public
+
+  def raw
+      @raw
+  end
+
+  def version
+      @version
+  end
+
+  def frames
+      @frames
+  end
+
+  alias old_set []=
+  private :old_set
+  
+
+  def []=(key,val)
+   if @version == ""
+     raise ArgumentError, "undefined version of ID3-tag! - set version before accessing components!\n" 
+   else
+     if ID3::SUPPORTED_SYMBOLS[@version].keys.include?(key)
+        old_set(key,val)
+     else 
+        # exception
+        raise ArgumentError, "Incorrect ID3-field \"#{key}\" for ID3 version #{@version}\n" +
+                             "\t\tvalid fields are: " + SUPPORTED_SYMBOLS[@version].keys.join(",") +"\n"
+     end
+   end
+  end
+
+  # keys, values, each, etc. come for free with Hash
 
 
   # ----------------------------------------------------------------------
@@ -338,18 +455,80 @@ class ID3tag < Hash
 
     ID3::SUPPORTED_SYMBOLS[@version].each{ |key,val|
        if val.type == Range
-          @field[key] = @raw[val].squeeze(" \000").chomp(" ").chomp("\000")
+          self[key] = @raw[val].squeeze(" \000").chomp(" ").chomp("\000")
        elsif val.type == Fixnum
-          @field[key] = @raw[val].to_s
+          self[key] = @raw[val].to_s
        else 
           printf "unknown key/val : #{key} / #{val}  ; val-type: %s\n", val.type
        end       
     }
     hastag
   end
+
+  # ----------------------------------------------------------------------
+  # parse_frame_header
+  #
+  # each frame consists of a header of fixed length; 
+  # depending on the ID3version, either 6 or 10 bytes.
+  # and of a data portion which is of variable length,
+  # and which contents might not be parsable by us
+  #
+  # INPUT:   index to where in the @raw data the frame starts
+  # RETURNS: if successful parse: 
+  #             total size in bytes, ID3frame struct
+  #          else:
+  #             0, nil
+  #
+  #
+  #          Struct of type ID3frame which contains:
+  #                the name, size (in bytes), headerX, 
+  #                dataStartX, dataEndX, flags
+  #          the data indices point into the @raw data, so we can cut out
+  #          and parse the data at a later point in time.
+  # 
+  #          total frame size = dataEndX - headerX
+  #          total header size= dataStartX - headerX
+  #          total data size  = dataEndX - dataStartX
+  #
+  private  
+  def parse_frame_header(x)
+     framename = ""; flags = nil
+     hsize = size = 0
+     if @version =~ "^2.2."
+        hsize = 6;                     # 2.2.x Header Size is 6 bytes
+        header = @raw[x..x+hsize-1]
+
+        framename = header[0..2]
+        size = (header[3]*256**2)+(header[4]*256)+header[5]
+        flags = nil
+     else 
+        # for version 2.3.0 and 2.4.0 the header is 10 bytes long
+        hsize = 10
+        header = @raw[x..x+hsize-1]
+
+        framename = header[0..3]
+        size = (header[4]*256**3)+(header[5]*256**2)+(header[6]*256)+header[7]
+        flags= header[8..9];        # perhaps we need to parse those flags later
+     end
+
+     # if this is a valid frame of known type, we return it's total length and a struct
+     # 
+     if ID3::SUPPORTED_SYMBOLS[@version].has_value?(framename)
+         if !defined? @frames
+            @frames = Hash.new ; # dynamically extending this ID3tag instance..
+         end
+         frame = ID3::ID3frame.new(framename, x, x+hsize , x+hsize + size - 1 , flags)
+         @frames[ @@framename2symbol[@version][frame.name] ] = frame
+         return size+hsize , frame
+     else
+         return 0, nil
+     end
+  end
+
+  public  
   # ----------------------------------------------------------------------
   # read_v2     reads a version 2.x ID3tag
-  
+
   def read_v2(filename)
     f = File.open(filename, 'r')
     hastag = (f.read(3) == "ID3")
@@ -362,91 +541,35 @@ class ID3tag < Hash
       f.seek(0)
       @raw = f.read(size) 
     else
-       @raw = @version = ""
+      @raw = @version = ""
     end
     f.close
+    #
+    # now parse all the frames
+    #
+    i = ID3v2headerSize; # we start parsing right after the ID3v2 header
+    
+    while (i < @raw.size) && (@raw[i] != 0)
+       len,frame = parse_frame_header(i)
+       if len != 0
+          i += len
+          self[ @@framename2symbol[@version][frame.name] ] = unpack_TXT(frame)
+       else
+          # finished parsing
+          break
+       end
+    end
+    
     hastag
   end
 
   # ----------------------------------------------------------------------
   # making data avaliable: 
 
-  def header
-      @header
-  end
-
-  def raw
-      @raw
-  end
-
-  def hastag
-      @hastag
-  end
-  def version
-      @version
-  end
-
 end 
 #
 ## -- Class ID3tag ---------------------------------------------------------------
 
-
-
-##### Class ID3frame #############################################################
-# 
-# ID3v2 frames implementation
-#
-#   - each frame consists of a header and a data section
-#   - the header is of fixed size (it's size is depending on the ID3 version,)
-#   - all frames in one ID3v2 header have the same size
-#   - 
-#
-#
-#
-class ID3frame
-
-  @header = ''
-  @data = ''
-  @size = 0
-  @id3version = 0
-
-
-  # INPUT  : filehandle, version
-  # RETURNS: nil            if  filehandle points to "0" bytes
-  #          frame object   if  frame header found
-  #
-  def initialize(data,version,offset=0)
-    
-#    if version =~ "1."
-#        raise exception
-        
-     if version == "2.0"
-        @headersize = 6
-        @name = data[0..2]
-        @size = (data[3]*256**2)+(data[2]*256)+data[3]
-     else 
-        @headersize = 10
-        @name = data[0..3]
-        @size = (data[4]*256**3)+(data[5]*256**2)+(data[6]*256)+data[7]
-        @flags= data[8..9]
-     end
-     @data = data[@headersize..(@headersize + @size - 1)]
-  end
-
-  def data
-     @data
-  end
-  def size
-     @size
-  end
-  def id3version
-     @id3version
-  end
-end 
-
-## -------------------------------------------------------------------------------
-
-end 
-
+end
 #   END of module ID3
 ##################################################################################
